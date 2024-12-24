993087b4dd16e93a3fcc189adfa1b7a1dc3a04f6
zhangyue66 <zhangyue66@baidu.com>
2024-12-24 18:03:32 +0800

    fix

--- libs/paddlex-hpi/src/paddlex_hpi/_config.py
+++ libs/paddlex-hpi/src/paddlex_hpi/_config.py
@@ -33,10 +33,10 @@ class _BackendConfig(BaseModel):
 class PaddleInferConfig(_BackendConfig):
     cpu_num_threads: int = 8
     enable_mkldnn: bool = True
-    precision: Literal["FP32", "FP16"] = "FP32"
     enable_trt: bool = False
     trt_dynamic_shapes: Optional[Dict[str, List[List[int]]]] = None
     trt_dynamic_shape_input_data: Optional[Dict[str, List[List[float]]]] = None
+    trt_precision: Literal["FP32", "FP16"] = "FP32"
     enable_log_info: bool = False
 
     def update_ui_option(self, option: ui.RuntimeOption, model_dir: Path) -> None:
@@ -44,7 +44,6 @@ class PaddleInferConfig(_BackendConfig):
         option.set_cpu_thread_num(self.cpu_num_threads)
         option.paddle_infer_option.enable_mkldnn = self.enable_mkldnn
         option.paddle_infer_option.enable_trt = self.enable_trt
-        option.trt_option.serialize_file = str(model_dir / "trt_serialized.trt")
         if self.trt_dynamic_shapes is not None:
             for name, shapes in self.trt_dynamic_shapes.items():
                 option.trt_option.set_shape(name, *shapes)
@@ -52,9 +51,10 @@ class PaddleInferConfig(_BackendConfig):
             for name, data in self.trt_dynamic_shape_input_data.items():
                 option.trt_option.set_input_data(name, *data)
         if self.enable_trt:
+            option.trt_option.serialize_file = str(model_dir / "trt_serialized.trt")
             option.paddle_infer_option.collect_trt_shape = True
             option.paddle_infer_option.collect_trt_shape_by_device = True
-            if self.precision == "FP16":
+            if self.trt_precision == "FP16":
                 option.trt_option.enable_fp16 = True
         option.paddle_infer_option.enable_log_info = self.enable_log_info
 
@@ -82,11 +82,11 @@ class TensorRTConfig(_BackendConfig):
     def update_ui_option(self, option: ui.RuntimeOption, model_dir: Path) -> None:
         option.use_trt_backend()
         option.trt_option.serialize_file = str(model_dir / "trt_serialized.trt")
+        if self.precision == "FP16":
+            option.trt_option.enable_fp16 = True
         if self.dynamic_shapes is not None:
             for name, shapes in self.dynamic_shapes.items():
                 option.trt_option.set_shape(name, *shapes)
-        if self.precision == "FP16":
-            option.trt_option.enable_fp16 = True
 
 
 class PaddleTensorRTConfig(_BackendConfig):
