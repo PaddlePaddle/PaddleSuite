f9f20f1a8e33cdd6026181b9efc0e7d794daed62
zhangyutian02 <zhangyutian02@baidu.com>
2024-12-31 17:37:37 +0800

    renew

--- paddlex/utils/device.py
+++ paddlex/utils/device.py
@@ -36,7 +36,7 @@ def get_default_device():
         # maybe edge devices like Jetson
         if os.path.exists("/etc/nv_tegra_release"):
             avail_gpus = [0]
-    # print(f"* DEBUG: avail_gpus = {avail_gpus}")
+            print("Detected that the current device is a Jetson edge device. Since GPUtil cannot detect GPU on Jetson, the function defaults to returning GPU: 0")
     if not avail_gpus:
         return "cpu"
     else:
