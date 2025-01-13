826237346df6084d89b5fda5598f71b6d02bda92
CV-LS <2690170518@qq.com>
2025-01-13 07:53:45 +0000

    support markdown

--- paddlex/inference/__init__.py
+++ paddlex/inference/__init__.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from ..utils import logging
 from ..utils.flags import USE_NEW_INFERENCE, NEW_PREDICTOR
 
