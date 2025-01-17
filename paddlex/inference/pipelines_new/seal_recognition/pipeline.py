c35469eb699be17c9df20cba9214f385d22f5724
liuhongen1234567 <2998388548@qq.com>
2025-01-17 08:10:22 +0000

    modify doc

--- paddlex/inference/pipelines_new/seal_recognition/pipeline.py
+++ paddlex/inference/pipelines_new/seal_recognition/pipeline.py
@@ -40,7 +40,6 @@ class SealRecognitionPipeline(BasePipeline):
         device: str = None,
         pp_option: PaddlePredictorOption = None,
         use_hpip: bool = False,
-        hpi_params: Optional[Dict[str, Any]] = None,
     ) -> None:
         """Initializes the seal recognition pipeline.
 
@@ -49,12 +48,9 @@ class SealRecognitionPipeline(BasePipeline):
             device (str, optional): Device to run the predictions on. Defaults to None.
             pp_option (PaddlePredictorOption, optional): PaddlePredictor options. Defaults to None.
             use_hpip (bool, optional): Whether to use high-performance inference (hpip) for prediction. Defaults to False.
-            hpi_params (Optional[Dict[str, Any]], optional): HPIP parameters. Defaults to None.
         """
 
-        super().__init__(
-            device=device, pp_option=pp_option, use_hpip=use_hpip, hpi_params=hpi_params
-        )
+        super().__init__(device=device, pp_option=pp_option, use_hpip=use_hpip)
 
         self.use_doc_preprocessor = config.get("use_doc_preprocessor", True)
         if self.use_doc_preprocessor:
@@ -182,7 +178,9 @@ class SealRecognitionPipeline(BasePipeline):
                 input_path = f"{img_id}"
             else:
                 input_path = batch_data[0]
+
             image_array = self.img_reader(batch_data)[0]
+
             if model_settings["use_doc_preprocessor"]:
                 doc_preprocessor_res = next(
                     self.doc_preprocessor_pipeline(
